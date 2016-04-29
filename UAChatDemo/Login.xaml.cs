1b6d6a244ebc0bfffd165eb57b4cb4f173240b0b
Pham Hong Thai <hongthaipro@hotmail.com>
2016-04-29 16:19:09 +0700

    1

---
+++ UAChatDemo/Login.xaml.cs
@@ -0,0 +1,71 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using UAChatDemo.Models;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace UAChatDemo
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Login : Page
+    {
+        public Login()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+
+            myProgress.IsActive = !myProgress.IsActive;
+            btnLogin.IsEnabled = !btnLogin.IsEnabled;
+
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://auth.lhu.edu.vn/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            //client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/x-www-form-urlencoded"));
+            HttpContent requestContent = new StringContent("grant_type=password&client_id=dcff83ded88e4f26af0d88c483329e2f&username=" + txtUsername.Text + "&password=" + txtPassword.Password, Encoding.UTF8, "application/x-www-form-urlencoded");
+            HttpResponseMessage responseMessage = await client.PostAsync("lhu/token", requestContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                string jsonMessage;
+                using (Stream responseStream = await responseMessage.Content.ReadAsStreamAsync())
+                {
+                    jsonMessage = new StreamReader(responseStream).ReadToEnd();
+                }
+
+                TokenModel tokenResponse = (TokenModel)JsonConvert.DeserializeObject(jsonMessage, typeof(TokenModel));
+                var setting = ApplicationData.Current.LocalSettings;
+                setting.Values["AccessToken"] = tokenResponse.AccessToken;
+                Frame.Navigate(typeof(MainPage));
+            }
+            else
+            {
+                var dialog = new MessageDialog(responseMessage.ReasonPhrase);
+                await dialog.ShowAsync();
+            }
+            myProgress.IsActive = false;
+            btnLogin.IsEnabled = true;
+        }
+    }
+}
---
+++ UAChatDemo/MainPage.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UAChatDemo
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private bool incall = false, endoflist = false;
+        private int offset = 0;
+        public static ObservableCollection<string> History = new ObservableCollection<string>();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            lvHistory.ItemsSource = History;
+            fetchHstory(0);
+            
+
+        }
+
+        private async void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            var setting = ApplicationData.Current.LocalSettings;
+            if (setting.Values["AccessToken"] == null)
+            {
+                Frame.Navigate(typeof(Login));
+            }
+            else
+            {
+                var dialog = new MessageDialog(setting.Values["AccessToken"].ToString());
+                await dialog.ShowAsync();
+            }
+        }
+
+        public static ScrollViewer GetScrollViewer(DependencyObject depObj)
+        {
+            if (depObj is ScrollViewer) return depObj as ScrollViewer;
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(depObj); i++)
+            {
+                var child = VisualTreeHelper.GetChild(depObj, i);
+                var result = GetScrollViewer(child);
+                if (result != null) return result;
+            }
+            return null;
+        }
+
+        private void lvHistory_Loaded(object sender, RoutedEventArgs e)
+        {
+            ScrollViewer viewer = GetScrollViewer(this.lvHistory);
+            viewer.ViewChanged += Viewer_ViewChanged;
+        }
+
+        private void Viewer_ViewChanged(object sender, ScrollViewerViewChangedEventArgs e)
+        {
+            ScrollViewer view = (ScrollViewer)sender;
+            double progresss = view.VerticalOffset / view.ScrollableHeight;
+            if (progresss > 0.7 && !incall && !endoflist)
+            {
+                incall = true;
+                fetchHstory(++offset);
+            }
+        }
+
+        private void fetchHstory(int v)
+        {
+            int start = v * 20;
+            for (int i = start; i < start + 20; i++)
+            {
+
+            }
+        }
+    }
+}
